4c17cd4cf141070af2343d1c3bd9472d65bd48e5
justkooker <justkooker.stydy@gmail.com>
2024-03-04 14:44:31 +0200

    telegram echo

--- app.js
+++ app.js
@@ -1,35 +1,18 @@
-const { program } = require('commander');
 const TelegramBot = require("node-telegram-bot-api");
-const version = 'v1.0.0';
 const config = require('config');
 const TOKEN = config.get("token")
 const bot = new TelegramBot(TOKEN, { polling: true });
-const chatId = ''; //Тут має бути власний chatId, можна отримати командою @get_id_bot 
 
-program
-    .command('message')
-    .arguments('text')
-    .description('Send message to Telegram Bot')
-    .action((message) => bot.sendMessage(chatId, message)
-        .then(() => console.log('Message sent'))
-        .then(() => process.exit())
-        .catch((error) => console.log('Message submission error ', error)));
-program
-    .command('photo')
-    .arguments('photo')
-    .alias('p')
-    .description('Send photo to Telegram Bot. Just drag and drop it console after p-flag.')
-    .action((photo) => bot.sendPhoto(chatId, photo)
-        .then(() => console.log("Photo sent"))
-        .then(() => process.exit())
-        .catch((error) => console.log('Photo submission error', error)));
-
-program
-    .version(version, 'version,-v')
-    .description('version')
-    .action(() => {
-        console.log(version);
-        process.exit();
-    });
-
-program.parse(process.argv);
+bot.on('message', (message) => {
+    const chatId = message.chat.id;
+    const user = message.from.username;
+    const text = message.text;
+    if (message.text === 'photo') {
+        console.log(`Користувач ${user} запросив картинку`)
+        bot.sendMessage(chatId, `https://picsum.photos/200/300`);
+    } else {
+        console.log(`Користувач ${user} написав:${text}`);
+        bot.sendMessage(chatId, message.text)
+    }
+})
+//В завданні написано, треба використовувати axios, але його використання разов з node-telegram-bot-api тільки ускладнює код, на мою думку.
